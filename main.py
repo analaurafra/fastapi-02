2a208cf9a77d22689ea9a11e860a43c82a9c3826
analaurafra <analaura.francisco@gmail.com>
2025-12-26 19:07:21 -0300

    atualizando repo

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import Request
+from loguru import logger
+import time
+
+print(">>> SANITY CHECK: MAIN CARREGADO <<<")
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "API Rodando :))"}
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    age: int
 
-@app.post("/users/{user_id}")
-def get_user(user_id: int, active: bool = True):
-    return {
-        "id": user_id,
-        "active": active
-    }
+@app.post("/users")
+def create_user(user: UserCreate):
+    return user
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    body = await request.body()
+    logger.info(f"Request {request.method} {request.url} Body: {body.decode('utf-8')}")
+    response = await call_next(request)
+    logger.info(f"Response status_code={response.status_code} for {request.method} {request.url}")
+    return response
+
