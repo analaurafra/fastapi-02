df8df71f77b097b7aca783f4d33380f494814c97
analaurafra <analaura.francisco@gmail.com>
2025-12-26 11:54:43 -0300

    atualizando

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API rodando"}
+    return {"message": "API Rodando"}
 
 @app.post("/users/{user_id}")
 def get_user(user_id: int, active: bool = True):
