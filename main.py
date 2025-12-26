f0a7efd0b765afcc9a351663e74f930c17e3810f
analaurafra <analaura.francisco@gmail.com>
2025-12-26 11:41:02 -0300

    Initial FastAPI project with basic routes

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API rodando"}
+
+@app.post("/users/{user_id}")
+def get_user(user_id: int, active: bool = True):
+    return {
+        "id": user_id,
+        "active": active
+    }
